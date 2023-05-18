3493c100ea8189ad7ff0394780c63b50220cb5a0
Sldvlld <sladvlad@gmail.com>
2023-05-18 20:37:15 +0300

    delete log

--- src/API/contactsAPI.js
+++ src/API/contactsAPI.js
@@ -4,6 +4,5 @@ axios.defaults.baseURL = 'https://64660601228bd07b35596438.mockapi.io';
 
 export async function fetchContacts() {
   const response = await axios.get('/contacts');
-  console.log(response.data);
   return response.data;
 }

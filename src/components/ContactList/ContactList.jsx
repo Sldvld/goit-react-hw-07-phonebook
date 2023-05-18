2b9a57877b14e906b4ae20c0597674c68f50f59e
Sldvlld <sladvlad@gmail.com>
2023-05-18 20:39:24 +0300

    add api

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/contactsSlice';
-import { selectContacts, getContactFilter } from 'redux/selectors';
+// import { getContactFilter } from 'redux/selectors';
 import * as contactsOperations from '../../redux/contactsOperation';
 import * as contactsSelectors from '../../redux/selectors';
 import css from './ContactList.module.css';
@@ -9,7 +9,7 @@ import css from './ContactList.module.css';
 export function ContactList() {
   const dispatch = useDispatch();
   const contacts = useSelector(contactsSelectors.selectContacts);
-  const filter = useSelector(getContactFilter);
+  // const filter = useSelector(getContactFilter);
 
   // const filteredContacts = contacts.filter(contact =>
   //   contact.name.toLowerCase().includes(filter.toLowerCase())
